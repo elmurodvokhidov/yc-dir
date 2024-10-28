72eadb47afec2f7536ac5a58340e58be40bf4c1b
unknown <elmurodvkh@gmail.com>
2024-10-28 13:57:06 +0500

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <div className="text-2xl">Home</div>
+    <div className="">
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup, <br /> Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
+      </section>
+    </div>
   )
 }
