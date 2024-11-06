a12160abd7d2d5e77857fc8687981e95b98e1ba4
unknown <elmurodvkh@gmail.com>
2024-11-06 11:53:39 +0500

    startup details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -2,7 +2,7 @@ import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`
     *[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search ] | order(_createdAt desc) {
-  _id,
+    _id,
     title,
     slug,
     _createdAt,
@@ -20,7 +20,7 @@ export const STARTUPS_QUERY = defineQuery(`
 
 export const STARTUP_BY_ID_QUERY = defineQuery(`
   *[_type == "startup" && _id == $id][0]{
-  _id,
+    _id,
     title,
     slug,
     _createdAt,
@@ -36,4 +36,10 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`
     category,
     image,
     pitch
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+    _id,
+    views,
 }`);
