7440e9af67928899f53d0a6b5916f4c16921485a
unknown <elmurodvkh@gmail.com>
2024-11-12 19:02:38 +0500

    server action

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -32,15 +32,15 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
                     <div className="flex-between gap-5">
                         <Link href={`/user/${post.author?._id}`} className="flex gap-2 items-center mb-3">
                             <Image
-                                src={post.author.image}
+                                src={post.author?.image}
                                 alt="avatar"
                                 width={65}
                                 height={64}
                                 className="rounded-full drop-shadow-lg"
                             />
                             <div>
-                                <p className="text-20-medium">{post.author.name}</p>
-                                <p className="text-16-medium !text-black-300">{post.author.username}</p>
+                                <p className="text-20-medium">{post.author?.name}</p>
+                                <p className="text-16-medium !text-black-300">{post.author?.username}</p>
                             </div>
                         </Link>
 
